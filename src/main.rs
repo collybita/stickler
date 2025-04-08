1b49f0a0a2e2071bf9a5028c52c6e46955c9b12e
Benedek Horváth <benedekhorvath@edu.bme.hu>
2025-04-08 23:23:19 +0200

    add ui

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use app::App;
-use ratatui::crossterm::event::{DisableMouseCapture, EnableMouseCapture};
+use ui::ui;
+use ratatui::crossterm::event::{self, DisableMouseCapture, EnableMouseCapture, Event};
 use ratatui::crossterm::execute;
 use ratatui::crossterm::terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
 use ratatui::prelude::{Backend, CrosstermBackend};
@@ -8,6 +9,7 @@ use std::error::Error;
 use std::io;
 
 mod app;
+mod ui;
 
 fn main() -> Result<(), Box<dyn Error>> {
     enable_raw_mode()?;
@@ -31,5 +33,14 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<bool> {
-    todo!();
+    loop {
+        terminal.draw(|f| ui(f, app))?;
+
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Release {
+                // Skip events that are not KeyEventKind::Press
+                continue;
+            }
+        }
+    }
 }
