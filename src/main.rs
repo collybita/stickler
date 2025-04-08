73cfb173c0f5cd23793f13f0bbd1a3825de6679c
Benedek Horváth <benedekhorvath@edu.bme.hu>
2025-04-08 23:19:56 +0200

    add basic terminal setup and cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
-use ratatui::crossterm::event::EnableMouseCapture;
+use app::App;
+use ratatui::crossterm::event::{DisableMouseCapture, EnableMouseCapture};
 use ratatui::crossterm::execute;
-use ratatui::crossterm::terminal::{enable_raw_mode, EnterAlternateScreen};
+use ratatui::crossterm::terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
+use ratatui::prelude::CrosstermBackend;
+use ratatui::Terminal;
+use std::error::Error;
 use std::io;
 
 mod app;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     enable_raw_mode()?;
     let mut stderr = io::stderr();
-    execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;    
+    execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stderr);
+    let mut terminal = Terminal::new(backend)?;
+
+    let mut app = App::new();
+    let res = run_app(&mut terminal, &mut app);
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }
