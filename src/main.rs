1c54a7155a7cb345643dffe89c39933014681097
Benedek Horváth <benedekhorvath@edu.bme.hu>
2025-04-08 23:15:56 +0200

    add ratatui and basic setup in main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+use ratatui::crossterm::event::EnableMouseCapture;
+use ratatui::crossterm::execute;
+use ratatui::crossterm::terminal::{enable_raw_mode, EnterAlternateScreen};
+use std::io;
+
 mod app;
 
 fn main() {
-    
+    enable_raw_mode()?;
+    let mut stderr = io::stderr();
+    execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;    
 }
